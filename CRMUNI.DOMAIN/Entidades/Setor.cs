417dda5d7d2169c036cbb25bdb577e59bb71d8fa
erick.paludo <erickpaludoyt@gmail.com>
2026-09-17 13:54:52 -0300

    Um setor pode ter diversos funcionarios

--- CRMUNI.DOMAIN/Entidades/Setor.cs
+++ CRMUNI.DOMAIN/Entidades/Setor.cs
@@ -11,6 +11,7 @@ public sealed class Setor : EntidadeIdGuid
     public Empresa Empresa { get; }
     public NomeSetor Nome { get; private set; }
     public DescricaoSetor DescricaoSetor { get; private set; }
+    public List<Funcionario> Funcionarios { get; private set; } = new List<Funcionario>();
 
     private Setor(Empresa empresa, NomeSetor nome, DescricaoSetor descricao)
     {
