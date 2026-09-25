022fa60c199979df86d44c053f98d750745dd63c
erick.paludo <erickpaludoyt@gmail.com>
2026-09-25 19:49:42 -0300

    Propriedade Etapa adicionada

--- CRMUNI.DOMAIN/Entidades/Atendimento.cs
+++ CRMUNI.DOMAIN/Entidades/Atendimento.cs
@@ -9,8 +9,7 @@ public sealed class Atendimento : EntidadeIdInt
 {
     public Contato Contato { get; }
     public Funcionario Funcionario { get; private set; }
-    public EAtendimentoEtapa Etapa { get; private set; }
-    //TOdo usar entidade ETAPA
+    public Etapa Etapa { get; private set; }
     public EAtendimentoSituacao Situacao { get; private set; }
     //Todo tempo atendimento
 
@@ -18,18 +17,18 @@ public sealed class Atendimento : EntidadeIdInt
     public List<Mensagem> Mensagem { get; } = new();
     public List<Funcionario> Funcionarios { get; } = new(); //Pode ser redundante, ja que mensagem possui a entidade Funcionario, e vice versa
 
-    //valor ??
-    private Atendimento(Contato contato, Funcionario funcionario)
+    private Atendimento(Contato contato, Funcionario funcionario,Etapa etapa)
     {
         ValidaNulo.Verifica(contato,AtendimentoMensagens.PropriedadeNula("Contato"));
         ValidaNulo.Verifica(funcionario,AtendimentoMensagens.PropriedadeNula("Funcionario"));
+        ValidaNulo.Verifica(etapa,AtendimentoMensagens.PropriedadeNula("Etapa"));
         
         Contato = contato;
         Funcionario = funcionario;
-        Etapa = EAtendimentoEtapa.Inicial;
+        Etapa = etapa;
         Situacao = EAtendimentoSituacao.Aguardando;
     }
 
-    public static Atendimento Create(Contato contato, Funcionario funcionario)
-        => new(contato, funcionario);
+    public static Atendimento Create(Contato contato, Funcionario funcionario,Etapa etapa)
+        => new(contato, funcionario,etapa);
 }
