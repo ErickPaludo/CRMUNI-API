cdb7479471a25c6f2ae527dc1f78d2edccdae3ba
Erick Paludo <erickpaludoyt@gmail.com>
2026-09-26 09:53:00 -0300

    Ajuste codigo de erro agendamento

--- CRMUNI.DOMAIN/Validacoes/ObjetosValor/Atendimentos/Agendamentos/AgendamentoMesagens.cs
+++ CRMUNI.DOMAIN/Validacoes/ObjetosValor/Atendimentos/Agendamentos/AgendamentoMesagens.cs
@@ -3,12 +3,12 @@ namespace CRMUNI.DOMAIN.Validacoes.ObjetosValor.Atendimentos.Agendamentos;
 public static class AgendamentoMesagens
 {
     /// <summary>
-    /// Codigo de mensagem 0.8.x
+    /// Codigo de mensagem 5.8.x
     /// Valores de x.x.0 até x.x.10 são reservados para codigos "COMUNS"
     /// </summary>
     
-    public static string ValorNulo(string nome) => $"0.8.0 - {nome} não pode ser nullo";
+    public static string ValorNulo(string nome) => $"5.8.0 - {nome} não pode ser nullo";
 
-    public static string TempoMinimo(int minutos) => $"0.8.2 - O tempo mínimo do agendamento deve ser de {minutos} minutos";
+    public static string TempoMinimo(int minutos) => $"5.8.2 - O tempo mínimo do agendamento deve ser de {minutos} minutos";
 
 }
